c18dd6a1fd4c532d30f8524bd6d9190f34e2543b
Alvaro Garcia <derecerca@gmail.com>
2017-05-29 23:54:56 +0200

    Refactor: move class to its own file

--- src/test/java/com/example/kata/copyprogram/test/CopyShould.java
+++ src/test/java/com/example/kata/copyprogram/test/CopyShould.java
@@ -3,16 +3,10 @@ package com.example.kata.copyprogram.test;
 import com.example.kata.copyprogram.Copier;
 import com.example.kata.copyprogram.ReadKeyboard;
 import com.example.kata.copyprogram.WritePrinter;
-import org.jmock.AbstractExpectations;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
-import org.jmock.api.Action;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class CopyShould {
 
@@ -51,44 +45,4 @@ public class CopyShould {
                 .build();
     }
 
-    private static class CopyExpectations {
-        private String[] returnValues;
-        private WritePrinter to;
-        private ReadKeyboard from;
-
-        static CopyExpectations aNew() {
-            return new CopyExpectations();
-        }
-
-        CopyExpectations readingFrom(ReadKeyboard keyboardReader) {
-            this.from = keyboardReader;
-            return this;
-        }
-
-        CopyExpectations returning(String[] returnValues) {
-            this.returnValues = returnValues;
-            return this;
-        }
-
-        CopyExpectations writingTo(WritePrinter writePrinter) {
-            this.to = writePrinter;
-            return this;
-        }
-
-        Expectations build() {
-            return new Expectations() {{
-
-                List<Action> collect1 = Arrays.stream(returnValues).map(x->returnValue(true)).collect(Collectors.toList());
-                collect1.add(returnValue(false));
-                exactly(returnValues.length+1).of(from).hasNext();
-                will(onConsecutiveCalls(collect1.toArray(new Action[0])));
-                exactly(returnValues.length).of(from).get();
-                Action[] collect = Arrays.stream(returnValues).map(AbstractExpectations::returnValue).collect(Collectors.toList()).toArray(new Action[0]);
-                will(onConsecutiveCalls(collect
-                ));
-
-                Arrays.stream(returnValues).forEach(message -> oneOf(to).print(message));
-            }};
-        }
-    }
 }
