bd0c486652fc8af99a17eea4ddc073c119c0d183
Alvaro Garcia <derecerca@gmail.com>
2017-05-30 00:02:15 +0200

    Refactor: extract function

--- src/test/java/com/example/kata/copyprogram/test/CopyExpectations.java
+++ src/test/java/com/example/kata/copyprogram/test/CopyExpectations.java
@@ -8,6 +8,7 @@ import org.jmock.api.Action;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 class CopyExpectations {
@@ -35,22 +36,48 @@ class CopyExpectations {
     }
 
     Expectations build() {
-        return new Expectations() {{
+        return new Expectations() {
+            {
+                setUpLoopControl();
 
-            List<Action> collect1 = Arrays.stream(returnValues).map(x->returnValue(true)).collect(Collectors.toList());
-            collect1.add(returnValue(false));
-            exactly(returnValues.length+1).of(from).hasNext();
-            will(onConsecutiveCalls(array(collect1)));
-            exactly(returnValues.length).of(from).get();
-            Action[] collect = array(Arrays.stream(returnValues).map(AbstractExpectations::returnValue).collect(Collectors.toList()));
-            will(onConsecutiveCalls(collect
-            ));
+                setUpLoopValues();
 
-            Arrays.stream(returnValues).forEach(message -> oneOf(to).print(message));
-        }};
+                setUpDestination();
+            }
+
+            private void setUpDestination() {
+                Arrays.stream(returnValues).forEach(message -> oneOf(to).print(message));
+            }
+
+            private void setUpLoopValues() {
+                exactly(returnValues.length).of(from).get();
+                Function<String, Action> x = AbstractExpectations::returnValue;
+                Action[] collect = array(map(x));
+                will(onConsecutiveCalls(collect));
+            }
+
+            private void setUpLoopControl() {
+                Action[] valuesThatStopLoop = stopLoop();
+                exactly(returnValues.length + 1).of(from).hasNext();
+                will(onConsecutiveCalls((valuesThatStopLoop)));
+            }
+
+            private Action[] stopLoop() {
+                Function<String, Action> tautology = x -> returnValue(true);
+                List<Action> collect1 = map(tautology);
+                collect1.add(returnValue(false));
+                return array(collect1);
+            }
+
+            private List<Action> map(Function<String, Action> x) {
+                return (Arrays.stream(returnValues).map(x).collect(Collectors.toList()));
+            }
+
+
+            private Action[] array(List<Action> values) {
+                return values.toArray(new Action[0]);
+            }
+        };
     }
 
-    private Action[] array(List<Action> values) {
-        return values.toArray(new Action[0]);
-    }
 }
