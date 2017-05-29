d5e68caf93ecf3aecbe4817e2018a87c6cca732d
Alvaro Garcia <derecerca@gmail.com>
2017-05-29 23:29:36 +0200

    Red: output the only message

--- src/test/java/com/example/kata/copyprogram/test/CopyTest.java
+++ src/test/java/com/example/kata/copyprogram/test/CopyTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 
 public class CopyTest {
 
+    private WritePrinter writer;
     private Mockery context;
 
     private Copier sut;
@@ -21,6 +22,7 @@ public class CopyTest {
         context = new Mockery();
 
         keyboardReader = context.mock(ReadKeyboard.class);
+        writer= context.mock(WritePrinter.class);
 
         sut = new Copier(keyboardReader, null);
     }
@@ -29,6 +31,26 @@ public class CopyTest {
     public void does_not_output_when_there_is_nothing_on_input() {
         context.checking(new Expectations() {{
             oneOf(keyboardReader).hasNext(); will(onConsecutiveCalls(returnValue(false)));
+        }});
+
+        sut.copy();
+
+        context.assertIsSatisfied();
+    }
+
+
+    @Test
+    public void outputs_the_only_message() {
+        context.checking(new Expectations() {{
+            exactly(2).of(keyboardReader).hasNext(); will(onConsecutiveCalls(
+                    returnValue(true),
+                    returnValue(false)
+            ));
+            oneOf(keyboardReader).get(); will(onConsecutiveCalls(
+                    returnValue("a")
+            ));
+
+            oneOf(writer).print("a");
         }});
 
         sut.copy();
